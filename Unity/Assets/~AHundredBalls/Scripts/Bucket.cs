bb06a15c783c0537dbdae54e72240e9a2d31fde4
Guhe928 <sakamotooooo928@gmail.com>
2018-05-18 12:01:36 +1000

    Started AHundredBalls game

---
+++ Unity/Assets/~AHundredBalls/Scripts/Bucket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AHundredBalls
+{
+    public class Bucket : MonoBehaviour
+    {
+        public float speed = 10f;
+        public Vector2 direction = Vector2.right;
+
+        // Use this for initialization
+        void Update()
+        {
+            transform.position += (Vector3)direction * speed * Time.deltaTime;
+        }
+
+
+    }
+}
