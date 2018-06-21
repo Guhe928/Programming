00bcafa0368ba2fbba77ba39fb768dbf5bc3e4bb
Guhe928 <sakamotooooo928@gmail.com>
2018-06-22 09:18:15 +1000

    Finished Assessment.

---
+++ Unity/Assets/GuHe_Assessment1/Scripts 1/EndGoal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGoal : MonoBehaviour {
+
+    public GAMEMANAGERS gameManager;
+    
+    void OnTriggerEnter ()
+    {
+        gameManager.CompleteLevel();
+    }
+}
---
+++ Unity/Assets/GuHe_Assessment1/Scripts 1/GAMEMANAGERS.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAMEMANAGERS : MonoBehaviour {
+    void EndGame ()
+    {
+        Debug.Log("GAMEOVER");
+    }
+
+
+
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    
+}
